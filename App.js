ba8ac5c056408353c0380221a46279f620b66458
Abhijeet <abjoshi2005@gmail.com>
2025-08-14 12:32:03 +0530

    Done with episode 3

--- App.js
+++ App.js
@@ -1,38 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-/* <div id="parent">
-  <div id="child1">
-    <h1></h1>
-    <h2></h2>
-  </div>
-  <div id="child2">
-    <h1></h1>
-    <h2></h2>
-  </div>
-</div> */
-// to create stucture  like this first we have to create parent element in react like below
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "Child" }, [
-    React.createElement("h1", {}, "I am an h1 tag of child 1"),
-    React.createElement("h2", {}, "I am an h1 tag of child 1"),
-  ]),  
-  React.createElement("div", { id: "Child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag of child 2"),
-    React.createElement("h2", {}, "I am an h1 tag of child 2"),
-  ]),
-]);
-
-//as the above code becomes complex syntax jsx is used
-
-console.log(parent);
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const title = (
+   <h1 className="Head" tabIndex="1">
+    Namaste Abhi using JSX
+    
+  </h1>
+);
+
+
+const number = 100 ; 
+const HeadingComp = () => (
+  <div>
+    {title}
+    <h1>Inside HeadingComp</h1>
+  </div>
+);
+
+
+
+root.render([<HeadingComp/>]);
